08169fcfcbb1000a18e5fbf58fe4d3d241e937b2
Bolshekh <Bolshekh@mail.ru>
2025-06-02 18:48:26 +0700

    color changing textblock with event

--- AvaloniaTesting/AvaloniaTesting/MainWindow.axaml.cs
+++ AvaloniaTesting/AvaloniaTesting/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Media;
+using System;
 
 namespace AvaloniaTesting
 {
@@ -8,6 +10,48 @@ namespace AvaloniaTesting
 		{
 			
 			InitializeComponent();
+		}
+
+		private void TextBox_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+		{
+			Random _random = new Random();
+			int _r = _random.Next(0, 10);
+			switch (_r)
+			{
+				case 0:
+					WelcomeTextBox.Background = Brushes.Yellow;
+					break;
+				case 1:
+					WelcomeTextBox.Background = Brushes.Blue;
+					break;
+				case 2:
+					WelcomeTextBox.Background = Brushes.Green;
+					break;
+				case 3:
+					WelcomeTextBox.Background = Brushes.Gray;
+					break;
+				case 4:
+					WelcomeTextBox.Background = Brushes.Cyan;
+					break;
+				case 5:
+					WelcomeTextBox.Background = Brushes.SkyBlue;
+					break;
+				case 6:
+					WelcomeTextBox.Background = Brushes.LightBlue;
+					break;
+				case 7:
+					WelcomeTextBox.Background = Brushes.Aqua;
+					break;
+				case 8:
+					WelcomeTextBox.Background = Brushes.Orange;
+					break;
+				case 9:
+					WelcomeTextBox.Background = Brushes.Red;
+					break;
+				default:
+					WelcomeTextBox.Background = Brushes.White;
+					break;
+			}
 		}
 	}
 }
